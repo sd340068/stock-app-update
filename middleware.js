328d0c5a152642db506ba67bb91392e6d4cae6d6
sd340068 <86208105+sd340068@users.noreply.github.com>
2026-02-04 15:48:48 +0000

    Create middleware.js

---
+++ middleware.js
@@ -0,0 +1,22 @@
+import { NextResponse } from 'next/server'
+
+export function middleware(request) {
+  const authHeader = request.headers.get('authorization')
+
+  const USER = 'internal'
+  const PASS = 'supersecret'
+
+  const validAuth =
+    'Basic ' + Buffer.from(`${USER}:${PASS}`).toString('base64')
+
+  if (authHeader !== validAuth) {
+    return new Response('Authentication required', {
+      status: 401,
+      headers: {
+        'WWW-Authenticate': 'Basic realm="Internal Site"',
+      },
+    })
+  }
+
+  return NextResponse.next()
+}
